643283711a693015ca42e5a94194279e92bbe64f
Emrah Atalay <emrahatalay92@gmail.com>
2024-05-21 22:21:12 +0300

    Bölüm 16

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -5,7 +5,14 @@ import Pagination from '@/app/ui/invoices/pagination';
 import Table from '@/app/ui/invoices/table';
 import Search from '@/app/ui/search';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+import { Metadata } from 'next';
 import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+    title: 'Faturalar',
+    description: 'Faturalarınızı görüntüleyin ve yönetin. Faturalarınızı düzenleyin, silin ve oluşturun.',
+};
+
 
 export default async function Page({
     searchParams,
