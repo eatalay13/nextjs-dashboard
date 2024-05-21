d25e7508744ddf66cc9691eec24140bd8776306d
Emrah Atalay <emrahatalay92@gmail.com>
2024-05-21 22:55:31 +0300

    Customer Table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,31 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import Table from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Metadata } from "next";
+import { Suspense } from "react";
+
+export const metadata: Metadata = {
+    title: "Müşteriler",
+    description: "Müşterilerinizi görüntüleyin ve yönetin. Müşterilerinizi düzenleyin, silin ve oluşturun.",
+};
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+
+    return (
+        <div className="w-full">
+            <Suspense key={query} fallback={<InvoicesTableSkeleton />}>
+                <Table query={query} />
+            </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+
+            </div>
+        </div>
+    );
 }
--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,16 +1,15 @@
-import Image from 'next/image';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
-import {
-  CustomersTableType,
-  FormattedCustomersTable,
-} from '@/app/lib/definitions';
+import Image from 'next/image';
 
 export default async function CustomersTable({
-  customers,
+  query,
 }: {
-  customers: FormattedCustomersTable[];
+  query: string;
 }) {
+  const customers = await fetchFilteredCustomers(query)
+
   return (
     <div className="w-full">
       <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
