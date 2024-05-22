4fdb67b865a9be9941e4e5daca6c6035f990581e
Emrah Atalay <emrahatalay92@gmail.com>
2024-05-22 11:13:56 +0300

    refactoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '@/app/ui/global.css';
 
 import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
-import { PropsWithChildren, ReactNode } from 'react';
+import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
   title: {
@@ -13,17 +13,18 @@ export const metadata: Metadata = {
   creator: 'Emrah Atalay',
 };
 
-interface Props
-  extends PropsWithChildren<{
-    modal?: ReactNode;
-  }> {}
-
-export default function RootLayout(props: Props) {
+export default function RootLayout({
+  children,
+  modal,
+}: {
+  children: ReactNode;
+  modal?: ReactNode;
+}) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        {props.children}
-        {props.modal}
+        {children}
+        {modal}
       </body>
     </html>
   );
